a5aa6e7571a82013f7d1d7440c7393b05c2f0332
jagadeesh <vyomanuatjagadeesh@gmail.com>
2024-07-25 12:27:14 +0530

    Prepare for deployment

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Home.module.css';
-import raghuV from '../../Assets/Raghu.mp4'
+// import raghuV from '../../Assets/Raghu.mp4'
 
 const Home = () => {
   return (
@@ -8,9 +8,9 @@ const Home = () => {
       <div className={styles.HomeHeading}>
         <p>The soil is the great connector of our lives</p>
       </div>
-      <div className={styles.Video}>
+      {/* <div className={styles.Video}>
         <video loop autoPlay muted ><source src={raghuV} type='video/mp4'></source></video>
-      </div>
+      </div> */}
     </div>
   )
 }
